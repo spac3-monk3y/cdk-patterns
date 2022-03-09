35ddce3e9126cf2de810c5a99ed4474026841d67
Mauricio Villaescusa <maurovc@amazon.com>
2022-03-09 14:24:08 -0500

    initial commit

---
+++ .projenrc.js
@@ -0,0 +1,23 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Mauricio Villaescusa',
+  authorAddress: 'maurovc@amazon.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-patterns',
+  repositoryUrl: 'https://github.com/spac3-monk3y/cdk-patterns.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  // release: undefined,      /* Add release management to this project. */
+  // publishToPypi: {
+  //   distName: 'cdk-patterns',
+  //   module: 'cdk_patterns',
+  // },
+});
+
+project.addGitIgnore('.dev');
+
+project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="cdk-patterns.Hello"></a>
+
+#### Initializers <a name="Initializers" id="cdk-patterns.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'cdk-patterns'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#cdk-patterns.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="cdk-patterns.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
