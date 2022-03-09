736f32cc20343a97babd2c8de859bef8a5bc5f49
Mauricio Villaescusa <maurovc@amazon.com>
2022-03-09 15:03:39 -0500

    fix: remove publishing

--- .projenrc.js
+++ .projenrc.js
@@ -11,7 +11,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
-  // release: undefined,      /* Add release management to this project. */
+  release: false, /* Add release management to this project. */
   // publishToPypi: {
   //   distName: 'cdk-patterns',
   //   module: 'cdk_patterns',
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Common CDK Patterns
 
 ![Build](https://github.com/spac3-monk3y/cdk-patterns/workflows/build/badge.svg)
-![Release](https://github.com/spac3-monk3y/cdk-patterns/workflows/release/badge.svg)
+<!-- ![Release](https://github.com/spac3-monk3y/cdk-patterns/workflows/release/badge.svg) -->
